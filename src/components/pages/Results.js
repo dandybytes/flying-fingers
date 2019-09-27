da5b65e65aa6de171e3f41845db6e9b7c2570559
DandyBytes <dandybytes@gmx.com>
2019-09-27 10:39:53 +0300

    add Buttons to Test and Results page

--- src/components/pages/Results.js
+++ src/components/pages/Results.js
@@ -1,14 +1,33 @@
 import React, {useContext} from "react";
 import {Context} from "./../../state/State";
 import Table from "../common/Table";
+import Button from "../common/Button";
 import "./Results.css";
 
 const Results = () => {
-    const {results} = useContext(Context);
+    const {results, setCurrentPage} = useContext(Context);
     return (
-        <div>
-            <h1>Results Page</h1>
-            <Table title={"Characters"} data={results.chars} />
+        <div className="Results">
+            <header className="Results-header">
+                <h1>Results Page</h1>
+            </header>
+            <main>
+                <Table title={"Characters"} data={results.chars} />
+            </main>
+            <footer>
+                <Button
+                    text="Repeat Test"
+                    type="button"
+                    onClick={e => setCurrentPage("test")}
+                    style={{marginTop: "5vh"}}
+                />
+                <Button
+                    text="Take New Test"
+                    type="button"
+                    onClick={e => setCurrentPage("intro")}
+                    style={{marginTop: "5vh", marginLeft: "1vw"}}
+                />
+            </footer>
         </div>
     );
 };
--- src/components/pages/Test.js
+++ src/components/pages/Test.js
@@ -4,10 +4,11 @@ import {Context} from "../../state/State";
 import Timer from "../Timer";
 import TextBox from "../text/TextBox";
 import Keyboard from "../keyboard/Keyboard";
+import Button from "../common/Button";
 import "./Test.css";
 
 function Test() {
-    const {testStarted, timeLeft, initializeTextBox, setTimeLeft, handleKeyDown} = useContext(Context); //prettier-ignore
+    const {setCurrentPage, testStarted, timeLeft, initializeTextBox, setTimeLeft, handleKeyDown} = useContext(Context); //prettier-ignore
 
     const scrollCursorToMiddleOfTextBox = () => {
         const textBoxFrame = document.querySelector(".TextBox-Frame");
@@ -53,6 +54,20 @@ function Test() {
             <Timer />
             <TextBox />
             <Keyboard />
+            <footer>
+                <Button
+                    text="Back to Settings"
+                    type="button"
+                    onClick={e => setCurrentPage("intro")}
+                    style={{marginTop: "5vh"}}
+                />
+                <Button
+                    text="See Test Results"
+                    type="button"
+                    onClick={e => setCurrentPage("results")}
+                    style={{marginTop: "5vh", marginLeft: "1vw"}}
+                />
+            </footer>
         </div>
     );
 }
