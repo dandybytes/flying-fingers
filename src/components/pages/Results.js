70cf9aaa387024cd7a3be3d73d0eb0aa28d20498
DandyBytes <dandybytes@gmx.com>
2019-09-30 00:39:39 +0300

    customize data to appear on Results page; improve Results style

--- src/components/pages/Results.js
+++ src/components/pages/Results.js
@@ -6,15 +6,39 @@ import "./Results.css";
 
 const Results = () => {
     const {results, setCurrentPage} = useContext(Context);
+
+    const generalResults = {
+        "words per minute": results.words.wordPerMin,
+        accuracy: results.chars.accuracy.toFixed(2),
+        "characters per minute": results.chars.charPerMin,
+        "total words typed": results.words.total
+    };
+
+    const charResults = {
+        "total characters typed": results.chars.total,
+        "correctly typed characters": results.chars.correct,
+        "mistyped characters": results.chars.mistyped,
+        "corrected characters": results.chars.corrected
+    };
+
+    const wordResults = {
+        "total words typed": results.words.total,
+        "correctly typed words": results.words.correct,
+        "mistyped words": results.words.mistyped,
+        "adjusted speed (wpm)": (results.words.wordPerMin * results.chars.accuracy).toFixed(2)
+    };
+
     return (
         <div className="Results">
             <header className="Results-header">
-                <h1>Results Page</h1>
+                <h1>Test Results</h1>
             </header>
-            <main>
-                <Table title={"Characters"} data={results.chars} />
+            <main className="Results-main">
+                <Table title={"Total"} data={generalResults} />
+                <Table title={"Characters"} data={charResults} />
+                <Table title={"Words"} data={wordResults} />
             </main>
-            <footer>
+            <footer className="Results-footer">
                 <Button
                     text="Repeat Test"
                     type="button"
@@ -25,7 +49,7 @@ const Results = () => {
                     text="Take New Test"
                     type="button"
                     onClick={e => setCurrentPage("intro")}
-                    style={{marginTop: "5vh", marginLeft: "1vw"}}
+                    style={{marginTop: "5vh"}}
                 />
             </footer>
         </div>
