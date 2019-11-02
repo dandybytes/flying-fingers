956124b02ce9facabfa138f1a7b288e610525cb9
DandyBytes <dandybytes@gmx.com>
2019-11-02 17:58:57 +0200

    display warning modal on mobile devices

--- src/components/pages/Intro.js
+++ src/components/pages/Intro.js
@@ -1,14 +1,25 @@
-import React, {useContext} from "react";
+import React, {useContext, useState, useEffect} from "react";
 import {Context} from "../../state/State";
-import Typewriter from "./../common/Typewriter";
+import Typewriter from "../common/Typewriter";
 import DropDown from "../common/DropDown";
-import Button from "./../common/Button";
+import Button from "../common/Button";
+import Modal from '../common/Modal';
 import {durationOptions} from "../../data/settings";
 import "./Intro.css";
 
 const Intro = () => {
     const {testDuration, setTestDuration, setCurrentPage} = useContext(Context);
 
+    let [showModal, setShowModal] = useState(false);
+
+    // display warning about test incompatibility with mobile devices
+    useEffect(() => {
+        const isMobileDevice = window.navigator.userAgent.match(/Mobile|Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone/i);
+        const isSmallScreen = window.innerWidth < 500 || window.innerHeight < 750;
+        if (isMobileDevice || isSmallScreen) setShowModal(true);
+    }, [])
+
+    // select test duration
     const handleDropdownSelect = e => {
         e.target.blur();
         setTestDuration(Number(e.target.value));
@@ -33,6 +44,7 @@ const Intro = () => {
                 autoFocus={testDuration <= 0}
                 handleChange={handleDropdownSelect}
             />
+            
             <Button
                 className={testDuration <= 0 ? "Button Button-blocked" : "Button Button-animated"}
                 disabled={testDuration <= 0}
@@ -41,6 +53,22 @@ const Intro = () => {
                 onClick={handleTakeTestClick}
                 style={{marginTop: "5vh", visibility: testDuration <= 0 ? "hidden" : "visible"}}
             />
+
+            {showModal && (
+                <Modal showCross={false} message={""}>
+                    <h1 style={{margin: "0 0 1em", color: "firebrick", fontSize: "4vmin", fontWeight: 400, letterSpacing: 0, textTransform: "none"}}>This typing test does not currently support mobile devices.</h1>
+                    <Button
+                        text="Got it!"
+                        type="button"
+                        onClick={e => setShowModal(false)}
+                        style={{
+                            color: "white",
+                            backgroundColor: "green",
+                            fontSize: "3vmin"
+                        }}
+                    />
+                </Modal>
+            )}
         </div>
     );
 };
